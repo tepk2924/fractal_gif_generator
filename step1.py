6bac9fa031bbcf359d253a633b7ea58cbd9d126e
tepk2924 <chohan0925@gmail.com>
2025-02-28 01:12:16 +0900

    Added burning ship fractal option

--- step1.py
+++ step1.py
@@ -15,6 +15,8 @@ Img_height = configs['Img_height']
 Img_width = configs['Img_width']
 Iternum = configs['Iternum']
 
+Fractaltype = configs['FractalType']
+validtypes = ['mandelbrot', 'burningship']
 HURDLE = 2
 
 #Calculating iteration of fractal
@@ -25,11 +27,22 @@ complex_seed = Xcoors + Ycoors*1j
 complex_calc = np.zeros((Img_height, Img_width), dtype=np.complex128)
 calc_result = np.zeros((Img_height, Img_width), np.uint32)
 
-for idx in range(Iternum):
-    mask = np.where(np.abs(complex_calc) < HURDLE)
-    calc_result[mask] += 1
-    complex_calc[mask] = complex_calc[mask]**2 + complex_seed[mask]
-    print(f"Iteration {idx + 1}/{Iternum} complete")
+if Fractaltype == 'mandelbrot':
+    for idx in range(Iternum):
+        mask = np.where(np.abs(complex_calc) < HURDLE)
+        calc_result[mask] += 1
+        #mandelbrot iteration
+        complex_calc[mask] = complex_calc[mask]**2 + complex_seed[mask]
+        print(f"Iteration {idx + 1}/{Iternum} complete")
+elif Fractaltype == 'burningship':
+    for idx in range(Iternum):
+        mask = np.where(np.abs(complex_calc) < HURDLE)
+        calc_result[mask] += 1
+        #burningship iteration
+        complex_calc[mask] = (np.abs(complex_calc[mask].real) + np.abs(complex_calc[mask].imag)*1j)**2 + complex_seed[mask]
+        print(f"Iteration {idx + 1}/{Iternum} complete")
+else:
+    print(f"The fractal type should be one of the followings: {validtypes}")
 
 #Saving
 np.save(os.path.join(os.path.dirname(__file__), "fractal_calc.npy"), calc_result)
--- step2.py
+++ step2.py
@@ -26,17 +26,25 @@ image_names = [f"$Frame{idx + 1:0{digits}d}.png" for idx in range(Frames)]
 Img_height = intensity_raw.shape[0]
 Img_width = intensity_raw.shape[1]
 
+ColorCenterX = configs['ColorCenterX']
+ColorCenterY = configs['ColorCenterY']
+
+if ColorCenterX == 'center':
+    ColorCenterX = Img_width/2
+if ColorCenterY == 'center':
+    ColorCenterY = Img_height/2
+
 #Calculating Intensity Level
 intensity = np.where(intensity_raw == maxval, 0, (4*intensity_raw/maxval)**3)
 intensity = np.where(intensity > 1, 1, intensity)
 intensity = np.expand_dims(intensity, axis=2)
 
 #Generating Images
-x = np.tile(np.expand_dims(np.arange(Img_width), 0), (Img_height, 1)) - Img_width/2
-y = (np.tile(np.expand_dims(np.arange(Img_height), 0).T, (1, Img_width)) - Img_height/2)[::-1, :]
+x = np.tile(np.expand_dims(np.arange(Img_width), 0), (Img_height, 1)) - ColorCenterX
+y = (np.tile(np.expand_dims(np.arange(Img_height), 0).T, (1, Img_width)) - ColorCenterY)[::-1, :]
 for idx, (angleframeshift, image_name) in enumerate(zip(np.linspace(0, 2*np.pi, Frames), image_names)):
     angle = np.arctan2(y, x) + angleframeshift
-    H = np.expand_dims((HueSetting['Average'] + HueSetting['Deviation']*np.sin(HueSetting['Frequency']*angle + HueSetting['Shift'])).astype(np.uint8), 2)
+    H = np.expand_dims(((HueSetting['Average'] + HueSetting['Deviation']*np.sin(HueSetting['Frequency']*angle + HueSetting['Shift']))%180).astype(np.uint8), 2)
     S = np.expand_dims((SaturationSetting['Average'] + SaturationSetting['Deviation']*np.sin(SaturationSetting['Frequency']*angle + SaturationSetting['Shift'])).astype(np.uint8), 2)
     V = np.expand_dims((ValueSetting['Average'] + ValueSetting['Deviation']*np.sin(ValueSetting['Frequency']*angle + ValueSetting['Shift'])).astype(np.uint8), 2)
     img_np = (cv2.cvtColor(np.concatenate((H, S, V), axis=2), cv2.COLOR_HSV2RGB)*intensity).astype(np.uint8)
